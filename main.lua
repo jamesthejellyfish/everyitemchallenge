fb5abc6ca177a4409b131d1b909acf9d45011bd8
jamesthejellyfish <hungergamesnum.1@gmail.com>
2023-10-23 16:59:18 -0400

    edited the data script to work more efficiently.
    script should now work on all items, even those spawned after the room was entered.

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -----------------CONFIG------------------------------
 -----------------CONFIG------------------------------
 local challengename = "Every Item Challenge, changes every floor" --name on the challenges.xml
-local moddata = "[data]130,'Special:20:0:1:,2,'Special:229:'ITEM:1:1:,2,618667524,;304,'Special:149:0:1:,2,'Special:262:0:1:16:'ROOMSPAWN:,2,618667524,;130,'Special:20:0:1:,2,'Special:262:0:1:16:'ROOMSPAWN:,2,618667524,;252,'Special:4:0:0:6:'ROOMSPAWN:,2,'Special:226:'ITEM:1:16:'SPAWNED:,2,618667524,;252,'Special:262:0:0:21:'REMOVE:,2,'Special:233:'ITEM:1:1:,2,618667524,;252,'Special:262:0:0:25:'11dRFRPuNt0K1dRFRPuNt0KdRFRPuNt0K1dRFRPuNt0KdRFRPuNt0K1dRFRPuNt0KdRFRPuNt0K1dRFRPuNt0KdRFRPuNt0K1dRFRPuNt0K:,2,'Special:233:'ITEM:1:1:,2,618667524,;[/data]" --rules data for the challenge (copy from your savefile)
+local moddata = "[data]130,'Special:20:0:1:,2,'Special:229:'ITEM:1:1:,2,618667524,;304,'Special:149:0:1:,2,'Special:262:0:1:16:'ROOMSPAWN:,2,618667524,;130,'Special:20:0:1:,2,'Special:262:0:1:16:'ROOMSPAWN:,2,618667524,;252,'Special:4:0:0:6:'ROOMSPAWN:,2,'Special:226:'ITEM:1:16:'SPAWNED:,2,618667524,;252,'Special:262:0:0:5:'SPAWNED:,2,'Special:233:'ITEMdRFRPuNt0K1dRFRPuNt0K:1:16:'SPAWNED:,2,618667524,;[/data]" --rules data for the challenge (copy from your savefile)
 -----------------CONFIG------------------------------
 -----------------CONFIG------------------------------
 
